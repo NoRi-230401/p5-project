71156b46e68ffb09855614f9a2d91f27d9ef6f5a
NoRi <sedia2011-apple@yahoo.co.jp>
2026-03-09 20:37:06 +0900

    v01

--- sketch.js
+++ sketch.js
@@ -1,26 +1,66 @@
-let clickNo=0;
+const w = 500;
+const h = 500;
+const frate = 5;
+const nGear = 19;
+const bGear = 1.12;
+let rGear;
+let rSpace;
+let radGear;
 
-function setup(){
-  createCanvas(600, 400);
-  noLoop();
+function setup() {
+  createCanvas(w, h);
+  rGear = min(w, h) / 8;
+  rSpace = rGear + rGear * bGear;
+  radGear = TWO_PI / nGear;
+  frameRate(frate);
 }
 
-function draw(){
-  background(220);
-  ellipse(mouseX, mouseY, 80, 80);
+function draw() {
+  background(0);
 
-  text("mouseX:"+mouseX,10,20);
-  text("mouseY:"+mouseY,10,40);
-  text("clicked:"+clickNo,10,60);
-  
+  // Red
+  push();
+  stroke(color(200, 0, 0));
+  translate(w / 2, h / 2);
+  rotate(frameCount / frate);
+  drawGear(nGear, rGear);
+  pop();
+
+  // Green
+  push();
+  stroke(color(0, 200, 0));
+  translate(w / 2 - rSpace, h / 2);
+  rotate(-frameCount / frate + radGear / 2);
+  drawGear(nGear, rGear);
+  pop();
+
+  //Blue
+  push();
+  stroke(color(0, 0, 200));
+  translate(w / 2, h / 2 - rSpace);
+  rotate(-frameCount / frate);
+  drawGear(nGear, rGear);
+  pop();
+
+  //Sky Blue
+  push();
+  stroke(color(0, 255, 255));
+  // translate(rGear * 6 * 0.92, rGear * 6 * 0.92);
+  translate(w / 2 + rSpace / Math.sqrt(2), h / 2 + rSpace / Math.sqrt(2));
+  rotate(-frameCount / frate + 10);
+  drawGear(nGear, rGear);
+  pop();
 }
 
-
-function mousePressed(){
-  clickNo++;
-  redraw();
-
-  console.log("mouseX:"+mouseX);
-  console.log("mouseY:"+mouseY);
-  console.log("clicked:"+clickNo);
+function drawGear(n, r) {
+  for (i = 0; i < TWO_PI; i += 0.01) {
+    var y1 = 0;
+    if (sin(i * n) >= 0) {
+      y1 = r;
+    } else {
+      y1 = r * bGear;
+    }
+    line(y1 * cos(i), y1 * sin(i), 0, 0);
+  }
+  ellipse(0, 0, 10);
 }
